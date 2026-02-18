2ea53a53076e2bfbbd41eb5f6d44e6b8391dae47
kandatata111 <kandatata11@gmail.com>
2026-02-18 21:22:14 +0900

    Deploy: Update dashboard and fix requirements - 26/02/18 (水) 21:22:14.21

--- clear_history.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-DB_PATH = r'C:\Users\kanda\Desktop\PythonData\TradingViewWebhook\webhook_data.db'
-conn = sqlite3.connect(DB_PATH)
-conn.execute('DELETE FROM fire_history WHERE symbol = ?', ('USDJPY',))
-conn.commit()
-conn.close()
-print('Cleared fire history for USDJPY')
